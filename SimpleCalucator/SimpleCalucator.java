8233770946e1c05978457e60639e3d70e3fba841
Jonathan Akoi <Akoij0831@students.bowiestate.edu>
2025-10-07 16:15:06 -0400

    new one

---
+++ SimpleCalucator/SimpleCalucator.java
@@ -0,0 +1,11 @@
+public class SimpleCalucator {
+
+public static void main(String[] args) {
+
+    //perform calculation based on operation
+        if (5 > 3)
+        System.out.println("true");
+        else
+        System.out.println("false");
+    } 
+}
---
+++ To do list/ToDoList.java
@@ -0,0 +1,18 @@
+public class ToDoList {
+
+public static void main(String[] args) {
+  System.out.println("To Do List:");
+  System.out.println("1. Buy groceries");
+  System.out.println("2. Finish homework");
+  System.out.println("3. Call mom");
+  System.out.println("4. Clean the house");
+  System.out.println("5. Exercise");
+  }
+}
+
+
+
+
+
+
+
