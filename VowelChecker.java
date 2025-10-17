caefdfb861367396329d2988f3d5a2c3454b0807
Jonathan Akoi <Akoij0831@students.bowiestate.edu>
2025-10-17 07:04:02 -0400

    new

---
+++ VowelChecker.java
@@ -0,0 +1,19 @@
+package progress;
+
+public class VowelChecker {
+public static void main(String[] args){
+char ch = 'a';
+
+switch(ch){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        System.out.println(ch + " is a vowel.");
+        break;
+    default:
+        System.out.println(ch + " is not a vowel.");
+
+    }
+}
