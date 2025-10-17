be10221129bc33db96c89eee7c99263bf6b969cd
Jonathan Akoi <Akoij0831@students.bowiestate.edu>
2025-10-17 07:02:19 -0400

    New feature: Updated ToDoList button color and added new website files

---
+++ Website page/Scripts/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args) {
+        String input = "hey"; // example input
+
+        if(input.equals("hey")) {
+            System.out.println("welcome");
+        } else {
+            System.out.println("hey");
+        }
+    }
+}
